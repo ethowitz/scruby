ae3b09e703d89e231460974e0407469660cec3dd
Ethan Donowitz <ethowitz@users.noreply.github.com>
2019-11-03 01:16:37 -0400

    #22 Enforce method arg list length (#63)

--- src/main/scala/com/ethowitz/sruby/evaluator/RubyMethod.scala
+++ src/main/scala/com/ethowitz/sruby/evaluator/RubyMethod.scala
@@ -5,9 +5,17 @@ import com.ethowitz.sruby.core.RubyObject
 
 class RubyMethod(val params: Seq[Symbol], val ts: List[SyntaxTree]) {
   def invoke(args: Seq[RubyObject], evals: List[SyntaxTree] => EvaluationState): EvaluationState = {
-    val bindings: Map[Symbol, RubyObject] = (params zip args).toMap
+    args.length == params.length match {
+      case true =>
+        val bindings: Map[Symbol, RubyObject] = (params zip args).toMap
 
-    evals(ts.map(_.withBoundVars(bindings)))
+        evals(ts.map(_.withBoundVars(bindings)))
+      case false =>
+        val errorMessage =
+          s"wrong number of arguments (given ${args.length}, expected ${params.length})"
+
+        throw new Exception(errorMessage)
+    }
   }
 }
 
